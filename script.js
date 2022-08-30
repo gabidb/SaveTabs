774b1fecb54496c9625754f8106222f0cafa2235
Gabriela Georgieva <georgievaga@vmware.com>
2022-08-30 21:56:39 +0300

    final version

--- script.js
+++ script.js
@@ -3,25 +3,20 @@ document.getElementById("saveButton").addEventListener("click", getAllTabs);
 function getAllTabs() {
     chrome.windows.getCurrent(function(window){
         chrome.tabs.query({windowId:window.id},function(tabs){
-            chrome.bookmarks.create({
-                title: "Test",
-                url: null
-                }, bookmarkItem => {
-                    onBookmarkAdded(bookmarkItem);
-                    chrome.bookmarks.create({
-                        title: "Open All",
-                        url: 'javascript: (() => {})();',
-                        parentId: bookmarkItem.id
-                    }, onBookmarkAdded);
-                    tabs.forEach(function(tab){
-                        console.log(tab.url);
-                        chrome.bookmarks.create({
-                            title: tab.title,
-                            url: tab.url,
-                            parentId: bookmarkItem.id
-                        },onBookmarkAdded );
-                    });
+            var bmName = document.getElementById("bm_name").value;
+            var result = "javascript:(function(){"
+            var num = 1;
+            tabs.forEach(function(tab){
+                var url = `var w${num} = window.open('${tab.url}','_blank');`
+                num++;
+                result = result.concat(url);
             });
+            result = result.concat("})();");
+            console.log(result)
+            chrome.bookmarks.create({
+                title: bmName,
+                url: result,
+            },onBookmarkAdded);
         });
     });
    
@@ -29,4 +24,5 @@ function getAllTabs() {
 
 function onBookmarkAdded(bookmarkItem) {
     console.log("Bookmark added with ID: " + bookmarkItem.id, bookmarkItem);
+    window.close();
 }
